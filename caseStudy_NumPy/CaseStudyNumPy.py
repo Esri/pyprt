ebf83d2477cbef3737a7351bf753d4866f0bcc27
Camille Lechot <clechot@esri.com>
2019-04-25 15:44:13 +0200

    CE-6219: multi initial shapes solved

--- caseStudy_NumPy/CaseStudyNumPy.py
+++ caseStudy_NumPy/CaseStudyNumPy.py
@@ -17,7 +17,7 @@ def asset_file(filename):
 
 
 class Canvas(scene.SceneCanvas):
-    def __init__(self, generated_data, generated_data_faces , window_range_xmin, window_range_xmax, window_range_ymin, window_range_ymax, window_range_zmin, window_range_zmax):
+    def __init__(self, geometries_number, generated_data, generated_data_faces , window_range_xmin, window_range_xmax, window_range_ymin, window_range_ymax, window_range_zmin, window_range_zmax):
         scene.SceneCanvas.__init__(self, keys='interactive', size=(800, 550), show=True, title="Visualization of the generated model(s)")
 
         self.unfreeze()
@@ -27,32 +27,37 @@ class Canvas(scene.SceneCanvas):
         self.selected_point = None
         scene.visuals.GridLines(parent=self.view.scene)
 
-        if(generated_data_faces is not None):
-            mdata = MeshData(vertices = generated_data, faces = generated_data_faces)
+        for k in range(geometries_number):
+            if(generated_data_faces[k] is not None):
+                mdata = MeshData(vertices = generated_data[k], faces = generated_data_faces[k])
 
-            rng = np.random.RandomState(0)
-            nf = mdata.n_faces
-            fcolor = np.ones((nf, 4), dtype=np.float32)
-            someColors = [Color('green').RGBA, Color('green').RGBA ,Color('yellow').RGBA, Color('yellow').RGBA, Color('blue').RGBA, Color('blue').RGBA,Color('red').RGBA, Color('red').RGBA, Color('white').RGBA, Color('white').RGBA,Color('fuchsia').RGBA, Color('fuchsia').RGBA]
+                rng = np.random.RandomState(0)
+                nf = mdata.n_faces
+                fcolor = np.ones((nf, 4), dtype=np.float32)
+                someColors = [Color('green').RGBA ,Color('yellow').RGBA, Color('blue').RGBA,Color('red').RGBA, Color('white').RGBA,Color('fuchsia').RGBA]
 
-            for l in range(0,nf):
-                fcolor[l] = Color('green').RGBA #someColors[l]
+                for l in range(0,nf):
+                    k_ind = k
+                    if(k >= len(someColors)):
+                        k_ind -= len(someColors)
+                    fcolor[l] = someColors[k_ind]
 
-            #fcolor[0] = Color('blue').RGBA
-            mdata.set_face_colors(fcolor)
-            edge_v_index = mdata.get_edges()
+                #fcolor[0] = Color('blue').RGBA
+                mdata.set_face_colors(fcolor)
+                edge_v_index = mdata.get_edges()
 
-            mesh = scene.visuals.Mesh(meshdata = mdata)
-            
-            mesh.set_gl_state('additive', depth_test=False)
-            mlines = scene.visuals.Line(pos=generated_data,color='red',connect=edge_v_index)
+                mesh = scene.visuals.Mesh(meshdata = mdata)
+                
+                mesh.set_gl_state('additive', depth_test=False)
+                mlines = scene.visuals.Line(pos=generated_data[k],color='red',connect=edge_v_index)
 
-            self.view.add(mesh)
-            self.view.add(mlines)
+                self.view.add(mesh)
+                self.view.add(mlines)
 
-        s = scene.Markers(pos=generated_data)
+            s = scene.Markers(pos=generated_data[k])
 
-        self.view.add(s)
+            self.view.add(s)
+
         self.freeze()
 
 
@@ -74,7 +79,7 @@ if __name__ == '__main__':
     initialGeometry = pyprt.CustomGeometry(v)
     initialGeometry2 = pyprt.CustomGeometry(v2)
 
-    mod = pyprt.ModelGenerator([initialGeometry], asset_file("simple_rule2019.rpk"), ["ruleFile:string=bin/simple_rule2019.cgb", "startRule:string=Default$Footprint"], ["baseName:string=theModel"])
+    mod = pyprt.ModelGenerator([initialGeometry, initialGeometry2], asset_file("simple_rule2019.rpk"), ["ruleFile:string=bin/simple_rule2019.cgb", "startRule:string=Default$Footprint"], ["baseName:string=theModel"])
 
     if(mod.generate_model()):
         geo = mod.get_model_geometry()
@@ -94,39 +99,43 @@ if __name__ == '__main__':
 
 
     # Data
-    mat = geo_numpy[0].copy()
-    #mat[:, 1], mat[:, 2] = mat[:, 2], mat[:, 1].copy()
+    mat = geo_numpy.copy()
+    mat[:, :, 1], mat[:, :, 2] = mat[:, :, 2], mat[:, :, 1].copy()
     mat_f = []
 
-    xmin = np.amin(mat[:,0])
-    xmax = np.amax(mat[:,0])
-    ymin = np.amin(mat[:,1])
-    ymax = np.amax(mat[:,1])
-    zmin = np.amin(mat[:,2])
-    zmax = np.amax(mat[:,2])
+    xmin = np.amin(mat[:,:,0])
+    xmax = np.amax(mat[:,:,0])
+    ymin = np.amin(mat[:,:,1])
+    ymax = np.amax(mat[:,:,1])
+    zmin = np.amin(mat[:,:,2])
+    zmax = np.amax(mat[:,:,2])
 
-    mat_faces = []
-    for f in face_geo[0]:
-        if(len(f) == 3):
-            mat_faces.append(f)
-        elif(len(f) > 3):
-            for new_f in np.array(list(itertools.combinations(f,3))):
-                mat_faces.append(new_f)
-        elif(len(f) < 3):
-            # repeat one vertex index
-            current_ind = 0
-            new_f2 = []
-            while(len(new_f2) < 3):
-                new_f2.append(f[current_ind])
-                if(current_ind < len(f)-1):
-                    current_ind += 1
-                else:
-                    while(len(new_f2) < 3):
-                        new_f2.append(f[current_ind])
-                    current_ind += 1
+    for k in range(2):
+        mat_faces = []
+        for f in face_geo[k]:
+            if(len(f) == 3):
+                mat_faces.append(f)
+            elif(len(f) > 3):
+                # in vispy, 3 vertex indices per face
+                for new_f in np.array(list(itertools.combinations(f,3))):
+                    mat_faces.append(new_f)
+            elif(len(f) < 3):
+                # repeat one vertex index
+                current_ind = 0
+                new_f2 = []
+                while(len(new_f2) < 3):
+                    new_f2.append(f[current_ind])
+                    if(current_ind < len(f)-1):
+                        current_ind += 1
+                    else:
+                        while(len(new_f2) < 3):
+                            new_f2.append(f[current_ind])
+                        current_ind += 1
 
-            mat_faces.append(new_f2)
+                mat_faces.append(new_f2)
 
-    win = Canvas(mat,np.array(mat_faces), xmin, xmax, ymin, ymax, zmin, zmax)
+        mat_f.append(mat_faces)
+
+    win = Canvas(mat.shape[0],mat,np.array(mat_f), xmin, xmax, ymin, ymax, zmin, zmax)
     if sys.flags.interactive != 1:
         app.run()
