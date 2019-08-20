9d18fe5e566a755e4c33782a026f5017904e9472
Camille Lechot <clechot@esri.com>
2019-08-20 14:31:23 +0200

    CE-6973: missing thing in utility

--- src/utility.py
+++ src/utility.py
@@ -13,35 +13,44 @@ def collect_initial_shape_indices(report_or_matrix):
 def visualize_PRT_results(model, printing = False): ###
     if model:
         geometry_vertices = model.get_vertices()
-        initial_shape_idx = collect_initial_shape_indices(geometry_vertices)
-        # geo_summarized = summarize_matrix(geometry_vertices)
-
-        print("\nNumber of generated geometries (= nber of initial shapes):")
-        print(len(initial_shape_idx))
-
         rep_float = model.get_float_report()
         rep_string = model.get_string_report()
         rep_bool = model.get_bool_report()
         combined_rep = combine_reports(model)
 
+        if len(geometry_vertices) > 0:
+            initial_shape_idx = collect_initial_shape_indices(geometry_vertices)
+        elif len(combined_rep) > 0:
+            initial_shape_idx = collect_initial_shape_indices(combined_rep)
+        else:
+            return
+
+        print("\nNumber of generated geometries (= nber of initial shapes):")
+        print(len(initial_shape_idx))
+
         for i in initial_shape_idx:
+            print()
             print("Initial Shape Index: " + str(i))
 
-            geo_numpy = np.array(geometry_vertices[i])
-            geo_numpy_unique, indices = np.unique(np.around(geo_numpy,decimals=3), return_index = True, axis=0)
-            print("Size of the matrix containing the model vertices (no duplicates): " + str(geo_numpy[indices].shape))
+            if len(geometry_vertices) > 0:
+                print()
+                geo_numpy = np.array(geometry_vertices[i])
+                geo_numpy_unique, indices = np.unique(np.around(geo_numpy,decimals=3), return_index = True, axis=0)
+                print("Size of the matrix containing the model vertices (no duplicates): " + str(geo_numpy[indices].shape))
 
-            print("Size of the matrix containing the model faces: " + str(np.array(model.get_faces()[i]).shape))
+                print("Size of the matrix containing the model faces: " + str(np.array(model.get_faces()[i]).shape))
 
-            print("Report of the generated model:")
-            print(combined_rep[i])
+            if len(combined_rep) > 0 :
+                print()
+                print("Report of the generated model:")
+                print(combined_rep[i])
 
-        print()
-        if printing :
-            print("Reports of the generated model (floats report, strings report, bools report):")
-            print(rep_float)
-            print(rep_string)
-            print(rep_bool)
+                print()
+                if printing :
+                    print("Reports of the generated model (floats report, strings report, bools report):")
+                    print(rep_float)
+                    print(rep_string)
+                    print(rep_bool)
     else:
         print("\nError while instanciating the model generator.")
 
