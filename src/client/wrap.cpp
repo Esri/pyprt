25e72ca50539f9dcbb2d999ee26609e2b192481f
Camille Lechot <clechot@esri.com>
2019-09-05 11:22:21 +0200

    CE-6974: created header file for main bindings code

--- src/client/wrap.cpp
+++ src/client/wrap.cpp
@@ -18,6 +18,7 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 
+#include "wrap.h"
 #include "utils.h"
 #include "logging.h"
 #include "PyCallbacks.h"
@@ -61,147 +62,26 @@ const wchar_t* ENCODER_OPT_NAME = L"name";
 pcu::Path executablePath;
 
 
-// cstr must have space for cstrSize characters
-// cstr will be null-terminated and the actually needed size is placed in cstrSize
-void copyToCStr(const std::string& str, char* cstr, size_t& cstrSize) {
-	if (cstrSize > 0) {
-		strncpy(cstr, str.c_str(), cstrSize);
-		cstr[cstrSize - 1] = 0x0; // enforce null-termination
-	}
-	cstrSize = str.length()+1; // returns the actually needed size including terminating null
-}
-
-/**
-  * custom console logger to redirect PRT log events into the python output
-  */
-class PythonLogHandler : public prt::LogHandler {
-public:
-	PythonLogHandler() = default;
-	virtual ~PythonLogHandler() = default;
-
-	virtual void handleLogEvent(const wchar_t* msg, prt::LogLevel /*level*/) {
-		pybind11::print(L"[PRT]", msg);
-	}
-
-	virtual const prt::LogLevel* getLevels(size_t* count) {
-		*count = prt::LogHandler::ALL_COUNT;
-		return prt::LogHandler::ALL;
-	}
-
-	virtual void getFormat(bool* dateTime, bool* level) {
-		*dateTime = true;
-		*level = true;
-	}
-
-	virtual char* toXML(char* result, size_t* resultSize, prt::Status* stat = 0) const {
-		std::ostringstream out;
-		out << *this;
-		copyToCStr(out.str(), result, *resultSize);
-		if (stat) *stat = prt::STATUS_OK;
-		return result;
-	}
-
-	friend std::ostream& operator<<(std::ostream& stream, const PythonLogHandler&) {
-		stream << "<PythonLogHandler />";
-		return stream;
-	}
-
-private:
-	const prt::LogLevel* mLevels;
-	size_t mCount;
-};
-
-
-/**
-  * Helper struct to manage PRT lifetime (e.g. the prt::init() call)
-  */
-struct PRTContext {
-    PRTContext(prt::LogLevel minimalLogLevel, std::string const & sdkPath) {
-        executablePath = sdkPath.empty() ? pcu::getExecutablePath() : sdkPath;
-        const pcu::Path installPath = executablePath.getParent();
-
-        prt::addLogHandler(&mLogHandler);
-
-        // setup paths for plugins, assume standard SDK layout as per README.md
-        const pcu::Path extPath = installPath / "lib";
-
-        // initialize PRT with the path to its extension libraries, the default log level
-        const std::wstring wExtPath = extPath.native_wstring();
-        const std::array<const wchar_t*, 1> extPaths = { wExtPath.c_str() };
-        mPRTHandle.reset(prt::init(extPaths.data(), extPaths.size(), minimalLogLevel));
-    }
-
-    ~PRTContext() {
-        // shutdown PRT
-        mPRTHandle.reset();
-
-        // remove loggers
-        prt::removeLogHandler(&mLogHandler);
-    }
-
-    explicit operator bool() const {
-        return (bool)mPRTHandle;
-    }
-
-	PythonLogHandler mLogHandler;
-    pcu::ObjectPtr mPRTHandle;
-};
-
 namespace {
 
-std::unique_ptr<PRTContext> prtCtx;
+    std::unique_ptr<PRTContext> prtCtx;
 
-void initializePRT(std::string const & prtPath) {
-	if (!prtCtx) prtCtx.reset(new PRTContext(prt::LOG_DEBUG, prtPath));
-}
+    void initializePRT(std::string const & prtPath) {
+	    if (!prtCtx) prtCtx.reset(new PRTContext(prt::LOG_DEBUG, prtPath));
+    }
 
-bool isPRTInitialized() {
-	return (bool)prtCtx;
-}
+    bool isPRTInitialized() {
+	    return (bool)prtCtx;
+    }
 
-void shutdownPRT() {
-	prtCtx.reset();
-}
+    void shutdownPRT() {
+	    prtCtx.reset();
+    }
 
 } // namespace
 
 namespace py = pybind11;
 
-class Geometry {
-public:
-    Geometry(const std::vector<double>& vert);
-    Geometry() { }
-    ~Geometry() { }
-
-    void setGeometry(const std::vector<double>& vert, const size_t& vertCnt, const std::vector<uint32_t>& ind, const size_t& indCnt, const std::vector<uint32_t>& faceCnt, const size_t& faceCntCnt);
-    const double* getVertices() const { return vertices.data(); }
-    size_t getVertexCount() const { return vertexCount; }
-    const uint32_t* getIndices() const { return indices.data(); }
-    size_t getIndexCount() const { return indexCount; }
-    const uint32_t* getFaceCounts() const { return faceCounts.data(); }
-    size_t getFaceCountsCount() const { return faceCountsCount; }
-    
-protected:
-    std::vector<double> vertices;
-    size_t vertexCount;
-    std::vector<uint32_t> indices;
-    size_t indexCount;
-    std::vector<uint32_t> faceCounts;
-    size_t faceCountsCount;
-};
-
-Geometry::Geometry(const std::vector<double>& vert) {
-    vertices = vert;
-    vertexCount = vert.size();
-    indexCount = (size_t)(vertexCount / 3);
-    faceCountsCount = 1;
-
-    std::vector<uint32_t> indicesVector(indexCount);
-    std::iota(std::begin(indicesVector), std::end(indicesVector), 0);
-    indices = indicesVector;
-    std::vector<uint32_t> faceVector(1, (uint32_t)indexCount);
-    faceCounts = faceVector;
-}
 
 void Geometry::setGeometry(const std::vector<double>& vert, const size_t& vertCnt, const std::vector<uint32_t>& ind, const size_t& indCnt, const std::vector<uint32_t>& faceCnt, const size_t& faceCntCnt) {
     vertices = vert;
@@ -212,30 +92,6 @@ void Geometry::setGeometry(const std::vector<double>& vert, const size_t& vertCn
     faceCountsCount = faceCntCnt;
 }
 
-class GeneratedGeometry {
-public:
-    GeneratedGeometry(const size_t& initialShapeIdx, const std::vector<double>& vertMatrix, const std::vector<std::vector<uint32_t>>& fMatrix, const FloatMap& floatRep, const StringMap& stringRep, const BoolMap& boolRep);
-    GeneratedGeometry() { }
-    ~GeneratedGeometry() { }
-
-    void convertGeometryIntoPythonStyle();
-
-    size_t getInitialShapeIndex() const { return initialShapeIdx; }
-    std::vector<std::vector<double>> getGenerationVertices() const { return verticesMatrix; }
-    std::vector<std::vector<uint32_t>> getGenerationFaces() const { return facesMatrix; }
-    FloatMap getGenerationFloatReport() const { return floatReportMap; }
-    StringMap getGenerationStringReport() const { return stringReportMap; }
-    BoolMap getGenerationBoolReport() const { return boolReportMap; }
-
-private:
-    size_t initialShapeIdx;
-    std::vector<double> verticesVect;
-    std::vector<std::vector<double>> verticesMatrix;
-    std::vector<std::vector<uint32_t>> facesMatrix;
-    FloatMap floatReportMap;
-    StringMap stringReportMap;
-    BoolMap boolReportMap;
-};
 
 GeneratedGeometry::GeneratedGeometry(const size_t& initShapeIdx, const std::vector<double>& vertMatrix, const std::vector<std::vector<uint32_t>>& fMatrix, const FloatMap& floatRep, const StringMap& stringRep, const BoolMap& boolRep) {
     initialShapeIdx = initShapeIdx;
@@ -255,37 +111,6 @@ void GeneratedGeometry::convertGeometryIntoPythonStyle() {
 
 
 namespace {
-    class ModelGenerator {
-    public:
-        ModelGenerator(const std::string& initShapePath);
-        ModelGenerator(const std::vector<Geometry>& myGeo);
-        ~ModelGenerator() { }
-
-        std::vector<GeneratedGeometry> generateModel(const std::string& rulePackagePath, py::dict shapeAttributes, py::dict encoderOptions, const wchar_t* encoderName);
-        std::vector<GeneratedGeometry> generateAnotherModel(py::dict shapeAttributes, py::dict encoderOptions);
-
-    private:
-        std::string initialShapePath;
-        std::vector<Geometry> initialGeometries;
-        pcu::ResolveMapPtr resolveMap;
-        pcu::CachePtr cache;
-
-        pcu::AttributeMapBuilderPtr encoderBuilder;
-        pcu::AttributeMapPtr CGAReportOptions;
-        pcu::AttributeMapPtr CGAPrintOptions;
-        pcu::AttributeMapPtr pyEncoderOptions;
-        std::vector<const prt::AttributeMap*> allEncodersOptions;
-        std::vector<const wchar_t*> allEncoders;
-
-        std::vector<pcu::InitialShapeBuilderPtr> initialShapesBuilders;
-
-        std::wstring ruleFile = L"bin/rule.cgb";
-        std::wstring startRule = L"default$init";
-        int32_t seed = 666;
-        std::wstring shapeName = L"InitialShape";
-
-        bool valid = true;
-    };
 
     ModelGenerator::ModelGenerator(const std::string& initShapePath) {
         initialShapePath = initShapePath;
