b2db7e5b214e19a5fcedcae6750290a1094722b2
Simon Haegler <shaegler@esri.com>
2019-11-07 16:37:07 +0100

    CE-7135 review: cleanup cmake statements, move 'make install' to install_lib

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 import os
-import re
 import sys
-import sysconfig
-import platform
 import subprocess
 
 from distutils.command.install_data import install_data
-from setuptools import setup, Extension, find_packages, Distribution
+from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 from setuptools.command.install_lib import install_lib
-from setuptools.command.test import test
-import pathlib
-import shutil
 import builtins
 
 builtins.__PYPRT_SETUP__ = True
 
+cmake_executable = 'cmake'
+cmake_build_type = "RelWithDebInfo"
+
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
+        super().__init__(name, sources=[])
         self.sourcedir = os.path.join(os.path.abspath(sourcedir), 'src')
 
 
@@ -30,36 +27,14 @@ class InstallCMakeLibsData(install_data):
 
 class InstallCMakeLibs(install_lib):
     def run(self):
-        self.announce("Moving library files", level=3)
-        self.skip_build = True
+        self.announce("Installing native extension", level=3)
 
-        build_bin_dir = os.path.join(self.distribution.bin_dir, 'bin')
-        build_lib_dir = os.path.join(self.distribution.bin_dir, 'lib')
-        os.makedirs(build_bin_dir, exist_ok=True)
-        os.makedirs(build_lib_dir, exist_ok=True)
+        cmake_install_command = [cmake_executable, "--build", self.distribution.cmake_build_dir, "--target", "install"]
+        if sys.platform.startswith('win32'):
+            cmake_install_command.append(["--config", cmake_build_type])
+        self.spawn(cmake_install_command)
 
-        install_dir = os.path.join(os.getcwd(), 'install', 'pyprt')
-        bin_dir = os.path.join(install_dir, 'bin')
-        lib_dir = os.path.join(install_dir, 'lib')
-
-        libs_bin = [os.path.join(bin_dir, _lib) for _lib in 
-                    os.listdir(bin_dir) if 
-                    os.path.isfile(os.path.join(bin_dir, _lib))]
-
-        libs_lib = [os.path.join(lib_dir, _lib) for _lib in 
-                    os.listdir(lib_dir) if 
-                    os.path.isfile(os.path.join(lib_dir, _lib))]
-
-        self.distribution.data_files = []
-
-        for l in libs_bin:
-            shutil.copyfile(l, os.path.join(build_bin_dir, os.path.basename(l)))
-            self.distribution.data_files.append(os.path.join(build_bin_dir, os.path.basename(l)))
-
-        for l in libs_lib:
-            shutil.copyfile(l, os.path.join(build_lib_dir, os.path.basename(l)))
-            self.distribution.data_files.append(os.path.join(build_lib_dir, os.path.basename(l)))
-
+        self.announce('Installing python modules', level=3)
         self.distribution.run_command("install_data")
         super().run()
 
@@ -77,25 +52,34 @@ class CMakeBuild(build_ext):
             self.build_cmake(ext)
 
     def build_cmake(self, extension: Extension):
-        self.announce("Preparing the build environment", level=3)
+        self.announce("Configuring CMake project", level=3)
 
-        build_dir = os.path.join(pathlib.Path(self.build_temp), '..', '..')
-        extension_path = pathlib.Path(self.get_ext_fullpath(extension.name))
+        cmake_install_prefix = os.path.join(self.build_lib, 'pyprt')
 
-        os.makedirs(pathlib.Path(self.build_temp), exist_ok=True)
-        os.makedirs(extension_path.parent.absolute(), exist_ok=True)
+        cmake_configure_command = [
+            cmake_executable,
+            '-DCMAKE_BUILD_TYPE={}'.format(cmake_build_type),
+            '-DCMAKE_INSTALL_PREFIX={}'.format(cmake_install_prefix),
+            '-DPYTHON_EXECUTABLE={}'.format(sys.executable),
+            '-H{}'.format(extension.sourcedir),
+            '-B{}'.format(self.build_temp)
+        ]
 
-        self.announce("Configuring cmake project", level=3)
+        if sys.platform.startswith('win32'):
+            cmake_configure_command.append('-Ax64')
+        elif sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
+            cmake_configure_command.append('-GNinja')
 
-        self.spawn(['cmake', '-H'+extension.sourcedir, '-B'+self.build_temp,
-                    '-A'+'x64','-DPYTHON_EXECUTABLE='+sys.executable,
-                    '-DCMAKE_BUILD_TYPE="RelWithDebInfo"'])
+        self.spawn(cmake_configure_command)
 
         self.announce("Building binaries", level=3)
-        self.spawn(["cmake", "--build", self.build_temp, "--target", "INSTALL", "--config", "RelWithDebInfo"])
+        cmake_build_command = [cmake_executable, "--build", self.build_temp]
+        if sys.platform.startswith('win32'):
+            cmake_build_command.append(["--config", cmake_build_type])
+        self.spawn(cmake_build_command)
 
-        self.distribution.bin_dir = os.path.join(extension_path.parent.absolute(), "pyprt")
-
+        # hack to transport cmake build dir from here to InstallCMakeLibs
+        self.distribution.cmake_build_dir = self.build_temp
 
 
 setup(
@@ -116,5 +100,5 @@ setup(
         'install_data': InstallCMakeLibsData,
         'install_lib': InstallCMakeLibs},
     zip_safe=False,
-    python_requires='>=3.6',
+    python_requires='>=3.6'
 )
