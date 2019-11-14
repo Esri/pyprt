248ed55999b9e7be2a853507c6ce74d54487d4ba
Camille Lechot <clechot@esri.com>
2019-11-14 15:54:53 +0100

    CE-7287: added/updated a test

--- caseStudy/test3.py
+++ caseStudy/test3.py
@@ -23,7 +23,8 @@ if not pyprt.is_prt_initialized():
 ### DATA
 rpk1 = asset_datafile('test_rule.rpk')
 attrs1 = {'ruleFile' : 'bin/test_rule.cgb', 'startRule' : 'Default$Footprint'}
-
+attrs2 = {'ruleFile' : 'bin/test_rule.cgb', 'startRule' : 'Default$Footprint', 'minBuildingHeight' : 30.0}
+attrs3 = {'ruleFile' : 'bin/test_rule.cgb', 'startRule' : 'Default$Footprint', 'text' : 'hello'}
 
 ### INITIAL SHAPES
 shape_geometry_1 = pyprt.Geometry([0, 0, 0,  0, 0, 100,  100, 0, 100,  100, 0, 0])
@@ -33,8 +34,11 @@ shape_geometry_2 = pyprt.Geometry([0, 0, 0,  0, 0, -10,  -10, 0, -10,  -10, 0, 0
 ### PRT GENERATION
 m1 = pyprt.ModelGenerator([shape_geometry_2, shape_geometry_1])
 
-mo = m1.generate_model(attrs1, rpk1, 'com.esri.pyprt.PyEncoder', {})#, {'emitReport' : False, 'emitGeometry' : False})
+mo = m1.generate_model([attrs1, attrs2], rpk1, 'com.esri.pyprt.PyEncoder', {})#, {'emitReport' : False, 'emitGeometry' : False})
 utility.visualize_PRT_results(mo)
+
+mo2 = m1.generate_model([attrs3])
+utility.visualize_PRT_results(mo2)
 
 
 ### PRT END
