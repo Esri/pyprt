e517b47a9ef621d120ad07355a467f0fcb66b746
Camille Lechot <clechot@esri.com>
2020-02-24 09:25:59 +0100

    added copyright statements

--- src/client/PyCallbacks.cpp
+++ src/client/PyCallbacks.cpp
@@ -1,5 +1,5 @@
 /**
- * ArcGIS CityEngine SDK Geometry Encoder for Python
+ * CityEngine SDK Geometry Encoder for Python
  *
  * Copyright (c) 2012-2020 Esri R&D Center Zurich
  *
@@ -14,6 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * A copy of the license is available in the repository's LICENSE file.
  */
 
 #include "PyCallbacks.h"
