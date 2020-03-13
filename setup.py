f11e1c3bb5480175a0b856341d3d627195fc4c56
Simon Haegler <shaegler@esri.com>
2020-03-13 13:16:39 +0100

    CE-7578: removed non-ascii character from pkg description

--- setup.py
+++ setup.py
@@ -38,11 +38,11 @@ pyprt_copyright = '(c) 2020, ' + pyprt_author
 pyprt_version = '1.0.0b1'  # keep consistent with __version__ in pyprt/__init__.py
 
 long_description = """
-PyPRT is a Python binding for PRT (“Procedural Runtime”). It enables the execution of [CityEngine](http://www.esri.com/software/cityengine) 
+PyPRT is a Python binding for PRT ("Procedural Runtime"). It enables the execution of [CityEngine](http://www.esri.com/software/cityengine) 
 CGA rules within Python. Using PyPRT, the generation of 3D content in Python is greatly simplified. Therefore, Python developers, data 
 scientists, GIS analysts, etc. can efficiently make use of CityEngine rule packages in order to create 3D geometries stored as Python data 
-structures, or to export these geometries in another format (like OBJ, Scene Layer Package, … ). Given an initial geometry, on which to 
-apply the CGA rule, the 3D generation is procedurally done in Python (Python script, Jupyter Notebook, …). This allows for efficient and 
+structures, or to export these geometries in another format (like OBJ, Scene Layer Package, ... ). Given an initial geometry, on which to 
+apply the CGA rule, the 3D generation is procedurally done in Python (Python script, Jupyter Notebook, ...). This allows for efficient and 
 customizable geometry generation. For instance, when modeling buildings, PyPRT users can easily change the parameters of the generated 
 buildings (like the height or the shape) by changing the values of the CGA rule input attributes.
 
@@ -54,7 +54,7 @@ shapes. Moreover, the outputted 3D geometries can either be used inside Python o
 
 *PyPRT is free for non-commercial use.* Commercial use requires at least one commercial license of the latest CityEngine version installed 
 in the organization. No redistribution is allowed. An exception is the PyPRT source code (without CityEngine SDK, binaries or object code), 
-which is licensed under the Apache License, Version 2.0 (the “License”); you may not use this work except in compliance with the License. 
+which is licensed under the Apache License, Version 2.0 (the "License"); you may not use this work except in compliance with the License. 
 You may obtain a copy of the License at [http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0).
 """
 
