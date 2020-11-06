53e6c89e77a9a7abb8fd8af4aef48179b7851775
Camille Lechot <clechot@esri.com>
2020-11-06 20:41:10 +0100

    filter out special "streetWidth(a)" rule attribute

--- src/client/api.cpp
+++ src/client/api.cpp
@@ -132,6 +132,8 @@ py::dict getRuleAttributes(const prt::RuleFileInfo* ruleFileInfo) {
 		const std::wstring fullName(attr->getName());
 		if (fullName.find(L"Default$") != 0)
 			continue;
+		if (fullName.find(L"streetWidth") != std::wstring::npos && attr->getNumParameters() == 1)
+			continue;
 		const std::wstring name = fullName.substr(8);
 		getAnnotations(attr, annotations, hidden);
 
