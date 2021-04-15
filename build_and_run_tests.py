feb3b2b77dff29dd170f05986d4a102f3eaaf97a
Simon Haegler <shaegler@esri.com>
2021-04-15 10:07:31 +0200

    updated standalone test build/run script for Python 3.8

--- build_and_run_tests.py
+++ build_and_run_tests.py
@@ -1,21 +1,25 @@
 import os
+import sys
 import tempfile
 import platform
 import venv
 
+env_os = "windows" if platform.system() == "Windows" else "centos7"
+env_py = f"py{sys.version_info[0]}{sys.version_info[1]}"
+
 env_dir = tempfile.TemporaryDirectory(prefix="pyprt-test-venv")
 venv.create(env_dir.name, with_pip=True)
 
-bin_segment = 'Scripts' if platform.system() == "Windows" else 'bin'
+bin_segment = "Scripts" if platform.system() == "Windows" else "bin"
 py_cmd = os.path.join(env_dir.name, bin_segment, 'python')
 
-os.system("{} -m pip install --upgrade pip".format(py_cmd))
-os.system("{} -m pip install --upgrade wheel".format(py_cmd))
-os.system("{} -m pip install -r requirements.txt".format(py_cmd))
+os.system(f"{py_cmd} -m pip install --upgrade pip")
+os.system(f"{py_cmd} -m pip install --upgrade wheel")
+os.system(f"{py_cmd} -m pip install -r envs/{env_os}/{env_py}/requirements.txt")
 
-os.system("{} setup.py clean --all".format(py_cmd))
-os.system("{} setup.py install".format(py_cmd))
-os.system("{} tests/run_tests.py".format(py_cmd))
-os.system("{} setup.py clean --all".format(py_cmd))
+os.system(f"{py_cmd} setup.py clean --all")
+os.system(f"{py_cmd} setup.py install")
+os.system(f"{py_cmd} tests/run_tests.py")
+os.system(f"{py_cmd} setup.py clean --all")
 
 env_dir.cleanup()
