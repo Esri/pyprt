6d1ebf9a9621a2c8dc9935df2db02b58120ea7e1
Camille Lechot <clechot@esri.com>
2021-08-24 18:44:53 +0200

    Update tests script with new envs folder structure

--- build_and_run_tests.py
+++ build_and_run_tests.py
@@ -5,7 +5,7 @@ import platform
 import venv
 
 env_os = "windows" if platform.system() == "Windows" else "centos7"
-env_py = f"py{sys.version_info[0]}{sys.version_info[1]}"
+env_py = f"py{sys.version_info[0]}.{sys.version_info[1]}"
 
 env_dir = tempfile.TemporaryDirectory(prefix="pyprt-test-venv")
 venv.create(env_dir.name, with_pip=True)
@@ -15,7 +15,7 @@ py_cmd = os.path.join(env_dir.name, bin_segment, 'python')
 
 os.system(f"{py_cmd} -m pip install --upgrade pip")
 os.system(f"{py_cmd} -m pip install --upgrade wheel")
-os.system(f"{py_cmd} -m pip install -r envs/{env_os}/{env_py}/requirements.txt")
+os.system(f"{py_cmd} -m pip install -r envs/{env_os}/wheel/requirements-{env_py}.txt")
 
 os.system(f"{py_cmd} setup.py clean --all")
 os.system(f"{py_cmd} setup.py install")
