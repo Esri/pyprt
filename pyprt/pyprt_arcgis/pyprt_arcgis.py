49dbd68e1f595cebf58db48d481024c7170ca377
cami9495 <clechot@esri.com>
2022-11-03 15:15:03 +0100

    Initial commit: copy code from example 10

--- pyprt/pyprt_arcgis/pyprt_arcgis.py
+++ pyprt/pyprt_arcgis/pyprt_arcgis.py
@@ -27,7 +27,7 @@ except ModuleNotFoundError:
 
 def add_dimension(array_coord_2d):
     array_coord_3d = np.insert(array_coord_2d, 1, 0, axis=1)
-    return np.reshape(array_coord_3d, (1, array_coord_3d.shape[0]*array_coord_3d.shape[1]))
+    return np.reshape(array_coord_3d, (1, array_coord_3d.shape[0] * array_coord_3d.shape[1]))
 
 
 def swap_yz_dimensions(array_coord):
@@ -35,20 +35,49 @@ def swap_yz_dimensions(array_coord):
     temp = np.copy(array_coord[:, 1])
     coord_swap_dim[:, 1] = coord_swap_dim[:, 2]
     coord_swap_dim[:, 2] = temp
-    return np.reshape(coord_swap_dim, (1, coord_swap_dim.shape[0]*coord_swap_dim.shape[1]))
+    return np.reshape(coord_swap_dim, (1, coord_swap_dim.shape[0] * coord_swap_dim.shape[1]))
+
+
+def get_hole_info(geo):
+    # use Shapely to detect interior rings (holes)
+    # doing the ccw check is a workaround as sometimes the exterior is actually a hole
+    # we assume that interior rings follow "their" exterior ring
+
+    shapely_geo = geo.as_shapely
+    print(geo.type)
+    print(shapely_geo)
+
+    shapely_rings = []
+    for shapely_part in shapely_geo.geoms:
+        shapely_rings.append(shapely_part.exterior)
+        for shapely_interior in shapely_part.interiors:
+            shapely_rings.append(shapely_interior)
+
+    holes = []
+    hole = []
+    shapely_face_index = 0
+    for shapely_ring in shapely_rings:
+        if not shapely_ring.is_ccw:  # exterior
+            if len(hole) > 0:
+                holes.append(hole)
+            hole = [shapely_face_index]
+        else:  # interior
+            hole.append(shapely_face_index)
+        shapely_face_index += 1
+    if len(hole) > 0:
+        holes.append(hole)
+
+    return holes
 
 
 def arcgis_to_pyprt(feature_set):
     """arcgis_to_pyprt(feature_set) -> List[InitialShape]
     This function allows converting an ArcGIS FeatureSet into a list of PyPRT InitialShape instances.
     You then typically call the ModelGenerator constructor with the return value if this function as parameter.
-
     Parameters:
         feature_set: FeatureSet
-
     Returns:
         List[InitialShape]
-
     """
     initial_geometries = []
     for feature in feature_set.features:
@@ -58,15 +87,9 @@ def arcgis_to_pyprt(feature_set):
                 pts_cnt = 0
                 vert_coord_list = []
                 face_count_list = []
-                holes_ind_list = []
                 coord_list = geo.coordinates()
 
                 for face_idx, coord_part in enumerate(coord_list):
-                    if isinstance(coord_part, np.ndarray):
-                        in_geo = Geometry({"rings": [coord_part.tolist()]})
-                    else:
-                        in_geo = Geometry({"rings": [coord_part]})
-                    store_area = in_geo.area
                     coord_remove_last = coord_part[:-1]
                     coord_inverse = np.flip(coord_remove_last, axis=0)
                     coord_inverse[:, 1] *= -1
@@ -76,19 +99,18 @@ def arcgis_to_pyprt(feature_set):
                     elif len(coord_part[0]) == 3:
                         coord_fin = swap_yz_dimensions(coord_inverse)
                     else:
-                        print("Only 2D or 3D points are supported.")
+                        raise "Only 2D or 3D points are supported."
 
                     vert_coord_list.extend(coord_fin[0])
-                    nb_pts = len(coord_fin[0])/3
+                    nb_pts = len(coord_fin[0]) / 3
                     pts_cnt += nb_pts
                     face_count_list.append(int(nb_pts))
 
                 face_indices_list = list(range(0, sum(face_count_list)))
-                
-                initial_geometry = pyprt.InitialShape(vert_coord_list, face_indices_list, face_count_list)
+                holes = get_hole_info(geo)
+
+                initial_geometry = pyprt.InitialShape(vert_coord_list, face_indices_list, face_count_list, holes)
                 initial_geometries.append(initial_geometry)
         except:
-            print("This feature is not valid: ")
-            print(feature)
-            print()
+            print(f'Ignoring invalid feature with id = {feature.get_field("OBJECTID")}.')
     return initial_geometries
