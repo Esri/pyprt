2643437c3d58b1316167018c081aea884c262cab
Simon Haegler <shaegler@esri.com>
2025-03-16 17:38:32 +0100

    cleanup: extract common utils used by both client and codec

---
+++ src/cpp/common/logging.h
@@ -0,0 +1,104 @@
+/**
+ * PyPRT - Python Bindings for the Procedural Runtime (PRT) of CityEngine
+ *
+ * Copyright (c) 2012-2024 Esri R&D Center Zurich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * A copy of the license is available in the repository's LICENSE file.
+ */
+
+#pragma once
+
+#include "prt/API.h"
+
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+
+/**
+ * helper classes to redirect log events
+ */
+
+namespace logging {
+
+struct Logger {};
+
+const std::wstring LEVELS[] = {L"trace", L"debug", L"info", L"warning", L"error", L"fatal"};
+
+// log to std streams
+template <prt::LogLevel L>
+struct StreamLogger : public Logger {
+	explicit StreamLogger(std::wostream& out = std::wcout) : Logger(), mOut(out) {
+		mOut << prefix();
+	}
+	virtual ~StreamLogger() {
+		mOut << std::endl;
+	}
+	StreamLogger<L>& operator<<(std::wostream& (*x)(std::wostream&)) {
+		mOut << x;
+		return *this;
+	}
+	StreamLogger<L>& operator<<(const std::string& x) {
+		std::copy(x.begin(), x.end(), std::ostream_iterator<char, wchar_t>(mOut));
+		return *this;
+	}
+	template <typename T>
+	StreamLogger<L>& operator<<(const T& x) {
+		mOut << x;
+		return *this;
+	}
+	static std::wstring prefix() {
+		return L"[" + LEVELS[L] + L"] ";
+	}
+	std::wostream& mOut;
+};
+
+// log through the prt logger
+template <prt::LogLevel L>
+struct PRTLogger : public Logger {
+	PRTLogger() : Logger() {}
+	virtual ~PRTLogger() {
+		prt::log(wstr.str().c_str(), L);
+	}
+	PRTLogger<L>& operator<<(std::wostream& (*x)(std::wostream&)) {
+		wstr << x;
+		return *this;
+	}
+	PRTLogger<L>& operator<<(const std::string& x) {
+		std::copy(x.begin(), x.end(), std::ostream_iterator<char, wchar_t>(wstr));
+		return *this;
+	}
+	template <typename T>
+	PRTLogger<L>& operator<<(const T& x) {
+		wstr << x;
+		return *this;
+	}
+	std::wostringstream wstr;
+};
+
+// choose your logger (PRTLogger or StreamLogger)
+template <prt::LogLevel L>
+using LT = PRTLogger<L>;
+
+using _LOG_DBG = LT<prt::LOG_DEBUG>;
+using _LOG_INF = LT<prt::LOG_INFO>;
+using _LOG_WRN = LT<prt::LOG_WARNING>;
+using _LOG_ERR = LT<prt::LOG_ERROR>;
+
+} // namespace logging
+
+#define LOG_DBG logging::_LOG_DBG()
+#define LOG_INF logging::_LOG_INF()
+#define LOG_WRN logging::_LOG_WRN()
+#define LOG_ERR logging::_LOG_ERR()
