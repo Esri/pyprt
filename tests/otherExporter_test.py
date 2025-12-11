358ffa9773c3cee747a1caee46a2c84215db8768
Simon Haegler <shaegler@esri.com>
2025-12-11 14:44:48 +0100

    tests: switch test code to pytest
    included some small code cleanups

--- tests/otherExporter_test.py
+++ tests/otherExporter_test.py
@@ -14,7 +14,6 @@
 # A copy of the license is available in the repository's LICENSE file.
 
 import os
-import unittest
 
 import pyprt
 
@@ -29,28 +28,23 @@ def asset_output_file(filename):
     return os.path.join(os.path.dirname(CS_FOLDER), 'output', filename)
 
 
-class ExporterTest(unittest.TestCase):
-    def test_slpk(self):
-        if os.path.isfile(asset_output_file('Unittest4SLPK.slpk')):
-            os.remove(asset_output_file('Unittest4SLPK.slpk'))
+def test_slpk():
+    if os.path.isfile(asset_output_file('Unittest4SLPK.slpk')):
+        os.remove(asset_output_file('Unittest4SLPK.slpk'))
 
-        encoder_options = {
-            'outputPath': os.path.dirname(asset_output_file(''))}
-        os.makedirs(encoder_options['outputPath'], exist_ok=True)
+    encoder_options = {
+        'outputPath': os.path.dirname(asset_output_file(''))}
+    os.makedirs(encoder_options['outputPath'], exist_ok=True)
 
-        shape_geo_from_obj = pyprt.InitialShape(
-            asset_file('building_parcel.obj'))
-        rpk = asset_file('envelope2002.rpk')
-        attrs = {'report_but_not_display_green': True}
-        slpk_options = {'layerTextureEncoding': ['2'], 'layerEnabled': [True], 'layerUID': ['1'],
-                        'layerName': ['Salut'], 'layerTextureQuality': [1.0], 'layerTextureCompression': [9],
-                        'layerTextureScaling': [1.0], 'layerTextureMaxDimension': [2048],
-                        'layerFeatureGranularity': ['0'], 'layerBackfaceCulling': [False], 'baseName': 'Unittest4SLPK'}
-        slpk_options.update(encoder_options)
-        m = pyprt.ModelGenerator([shape_geo_from_obj])
-        m.generate_model(
-            [attrs], rpk, 'com.esri.prt.codecs.I3SEncoder', slpk_options)
-        self.assertTrue(os.path.isfile(
-            asset_output_file('Unittest4SLPK.slpk')))
-        self.assertGreater(
-            os.stat(asset_output_file('CGAReport.txt')).st_size, 0)
+    shape_geo_from_obj = pyprt.InitialShape(asset_file('building_parcel.obj'))
+    rpk = asset_file('envelope2002.rpk')
+    attrs = {'report_but_not_display_green': True}
+    slpk_options = {'layerTextureEncoding': ['2'], 'layerEnabled': [True], 'layerUID': ['1'],
+                    'layerName': ['Salut'], 'layerTextureQuality': [1.0], 'layerTextureCompression': [9],
+                    'layerTextureScaling': [1.0], 'layerTextureMaxDimension': [2048],
+                    'layerFeatureGranularity': ['0'], 'layerBackfaceCulling': [False], 'baseName': 'Unittest4SLPK'}
+    slpk_options.update(encoder_options)
+    m = pyprt.ModelGenerator([shape_geo_from_obj])
+    m.generate_model([attrs], rpk, 'com.esri.prt.codecs.I3SEncoder', slpk_options)
+    assert os.path.isfile(asset_output_file('Unittest4SLPK.slpk'))
+    assert os.stat(asset_output_file('CGAReport.txt')).st_size > 0
